d0e80c2c7facc64a6db2af628f6a28583d9a6a3c
NovyAPP <micnowotnik@gmail.com>
2023-03-17 14:39:57 +0100

    created checkoutpage with checkout items

--- src/routes/navigation/navigation.component.tsx
+++ src/routes/navigation/navigation.component.tsx
@@ -2,7 +2,7 @@ import { useContext } from "react";
 import { Outlet, Link } from "react-router-dom";
 
 import CartIcon from "../../components/cart-icon/cart-icon.component";
-import CartDropdown from "../../components/cart-dropdown/cart-dropdown.ccomponent";
+import CartDropdown from "../../components/cart-dropdown/cart-dropdown.component";
 
 import { signOutUser } from "../../utils/firebase/firebase.utils";
 import { ReactComponent as CrwnLogo } from "../../assets/crown.svg";
